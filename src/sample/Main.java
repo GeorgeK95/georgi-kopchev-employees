0ebfd5a9872ca8cc18c143099ba0248e09a45689
GeorgeK95 <george_it@abv.bg>
2018-07-22 21:17:01 +0300

    Done file chooser and grid.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,90 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import sample.model.Person;
+
+import java.io.File;
 
 public class Main extends Application {
 
+    private TableView table = new TableView();
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Text Files", "*.txt")
+        );
+
         primaryStage.setTitle("Employees App");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setWidth(500);
+        primaryStage.setHeight(700);
+
+        final Label label = new Label("Address Book");
+        label.setFont(new Font("Arial", 20));
+
+//        table.setEditable(true);
+
+        TableColumn firstNameCol = new TableColumn("First Name");
+        firstNameCol.setMinWidth(100);
+        firstNameCol.setCellValueFactory(new PropertyValueFactory<Person, String>("firstName"));
+
+        TableColumn lastNameCol = new TableColumn("Last Name");
+        lastNameCol.setMinWidth(100);
+        lastNameCol.setCellValueFactory(new PropertyValueFactory<Person, String>("lastName"));
+
+        TableColumn emailCol = new TableColumn("Email");
+        emailCol.setMinWidth(200);
+        emailCol.setCellValueFactory(new PropertyValueFactory<Person, String>("email"));
+
+        table.getColumns().addAll(firstNameCol, lastNameCol, emailCol);
+
+        Button button = new Button("Select File");
+        button.setOnAction(e -> {
+            File file = fileChooser.showOpenDialog(primaryStage);
+            if (file != null) {
+                //here goes employees
+                //result from employees goes to the grid
+                String pesho1 = "Pesho";
+                String pesho2 = "Peshov";
+                String email = "pesho@abv.bg";
+
+                ObservableList<Person> data = FXCollections.observableArrayList(
+                        new Person(pesho1, pesho2, email)
+                );
+
+                table.setItems(data);
+
+                System.out.println(222);
+            }
+        });
+
+        final VBox vbox = new VBox(button);
+        vbox.setSpacing(5);
+        vbox.setPadding(new Insets(10, 0, 0, 10));
+        vbox.getChildren().addAll(label, table);
+
+        Scene scene = new Scene(new Group());
+
+        ((Group) scene.getRoot()).getChildren().addAll(vbox);
+
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
